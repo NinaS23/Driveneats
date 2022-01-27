05a1af3d78b9c4d8688982b38127fd6f95edb0de
maria <mariabevenuto062@gmail.com>
2022-01-27 13:30:14 -0300

    com js bugado

---
+++ script.js
@@ -0,0 +1,47 @@
+function escolherFrango() {
+  document.getElementById('frago').estyle.borderColor = 'green'
+  document.getElementById('camarão').estyle.borderColor = 'white'
+  document.getElementById('strogonoff').estyle.borderColor = 'white'
+}
+function escolherCamarao() {
+  document.getElementById('camarão').estyle.borderColor = 'green'
+  document.getElementById('frango').estyle.borderColor = 'white'
+  document.getElementById('strogonoff').estyle.borderColor = 'white'
+}
+function escolherStrogonoff() {
+  document.getElementById('strogonoff').estyle.borderColor = 'green'
+  document.getElementById('frango').estyle.borderColor = 'white'
+  document.getElementById('camarão').estyle.borderColor = 'white'
+}
+
+function escolherLimão() {
+  document.getElementById('limão').estyle.borderColor = 'green'
+  document.getElementById('laranja').estyle.borderColor = 'white'
+  document.getElementById('sprite').estyle.borderColor = 'white'
+}
+function escolherSprite() {
+  document.getElementById('limão').estyle.borderColor = 'white'
+  document.getElementById('laranja').estyle.borderColor = 'white'
+  document.getElementById('sprite').estyle.borderColor = 'green'
+}
+function escolherLaranja() {
+  document.getElementById('limão').estyle.borderColor = 'white'
+  document.getElementById('laranja').estyle.borderColor = 'green'
+  document.getElementById('sprite').estyle.borderColor = 'white'
+}
+
+function escolherMusse() {
+  document.getElementById('musse').estyle.borderColor = 'green'
+  document.getElementById('churrus').estyle.borderColor = 'white'
+  document.getElementById('bolinho').estyle.borderColor = 'white'
+}
+function escolherChurrus() {
+  document.getElementById('musse').estilos.borderColor = 'white'
+  document.getElementById('churrus').estilos.borderColor = 'green'
+  document.getElementById('bolinho').estilos.borderColor = 'white'
+}
+function escolherBolinho() {
+  document.getElementById('musse').estyle.borderColor = 'white'
+  document.getElementById('churrus').estyle.borderColor = 'white'
+  document.getElementById('bolinho').estyle.borderColor = 'green'
+}
